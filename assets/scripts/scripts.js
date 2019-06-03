b368e6cde58409a566385822a110de031c5ba01a
John Peart <john.peart@me.com>
2019-06-03 08:11:54 +0100

    Remove a ton of stuff

--- assets/scripts/scripts.js
+++
@@ -1,9 +0,0 @@
----
----
-
-function toggleMenu() {
-    var menuItems = document.getElementById("menu-items");
-    var menuToggle = document.getElementById("menu-toggle");
-    menuItems.classList.toggle("closed");
-    menuToggle.classList.toggle("open");
-}
